861d7babd1c4f8629ea79e211c289a53ebfef322
sverona <sasham@fastmail.com>
2018-12-21 21:41:08 -0600

    yagni the schema for now

--- Entities.py
+++ Entities.py
@@ -1,42 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy import *
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
 
-class Tournament(Base):
-    __tablename__ = "tournament"
-    id = Column(Integer, primary_key=True)
-    events = relationship("Event")
-    attendees = relationship("Attendee")
-
-
-class Event(Base):
-    __tablename__ = "event"
-    id = Column(Integer, primary_key=True)
-    tournament_id = Column(Integer, ForeignKey("tournament.id"))
-    phases = relationship("Phase")
-
-
-class Phase(Base):
-    __tablename__ = "phase"
-    id = Column(Integer, primary_key=True)
-    event_id = Column(Integer, ForeignKey("event.id"))
-    brackets = relationship("Bracket")
-
-
-class Bracket(Base):
-    __tablename__ = "bracket"
-    id = Column(Integer, primary_key=True)
-    phase_id = Column(Integer, ForeignKey("phase.id"))
-    sets = relationship("Set")
-
-
 class Set(Base):
     __tablename__ = "set"
     id = Column(Integer, primary_key=True)
-    bracket_id = Column(Integer, ForeignKey("bracket.id"))
     games = relationship("Game")
 
 
@@ -45,38 +16,39 @@ class Game(Base):
     id = Column(Integer, primary_key=True)
     set_id = Column(Integer, ForeignKey("set.id"))
     time_indices = relationship("TimeIndex")
+    ports = relationship("Ports")
+    sources = relationship("GameSource")
+
+
+class Source(Base):
+    __tablename__ = "source"
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+
+
+class GameSource(Base):
+    __tablename__ = "game_source"
+    game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
+    source_id = Column(Integer, ForeignKey("source.id"), primary_key=True)
+    source = relationship("Source")
+
+
+class Port(Base):
+    __tablename__ = "port"
+    game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
+    port_number = Column(Integer, primary_key=True)
+    portstates = relationship("PortState")
 
 
 class PortState(Base):
     __tablename__ = "portstate"
     id = Column(Integer, primary_key=True)
+    port_id = Column(Integer, ForeignKey("port.id"))
 
 
 class TimeIndex(Base):
     __tablename__ = "timeindex"
     id = Column(Integer, primary_key=True)
+    time = Column(Numeric)
     game_id = Column(Integer, ForeignKey("game.id"))
     port_states = Column(Integer, ForeignKey("portstate.id"))
-
-
-class Attendee(Base):
-    __tablename__ = "attendee"
-
-    player_id = Column(Integer, ForeignKey("player.id"), primary_key=True)
-    tournament_id = Column(Integer, ForeignKey("tournament.id"), primary_key=True)
-    player = relationship("Player")
-
-
-class GameSource(Base):
-    __tablename__ = "gamesource"
-
-    game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
-    source_id = Column(Integer, ForeignKey("source.id"), primary_key=True)
-    game = relationship("Game")
-
-
-class Source(Base):
-    __tablename__ = "source"
-
-    id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey("game.id"))
