ee32fa22c118b8bcbeaf8f914dd379b554c1532d
sverona <sasham@fastmail.com>
2019-01-07 22:31:34 -0600

    add args to segment.py

--- meleedb-segment/segment.py
+++ meleedb-segment/segment.py
@@ -8,20 +8,17 @@ from core.segmenter import Segmenter
 def __main__(args):
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("file", help="stream", type=str)
+    parser.add_argument("outfile", help="output file", type=str)
 
     args = parser.parse_args(args)
 
     stream = os.path.realpath(args.file)
-    print(stream)
+    outfile = os.path.realpath(args.outfile)
 
     match = Segmenter(stream)
     match.parse()
 
-    _, basename = os.path.split(stream)
-    basename, _ = os.path.splitext(basename)
-
-    outpath = os.path.join("data/{0}.json".format(basename))
-    with open(outpath, "w") as f:
+    with open(outfile, "w") as f:
         dump(match.data, f, default=lambda obj: obj.__dict__,
              indent=4, sort_keys=True)
 
