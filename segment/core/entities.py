ee5c8a628922bfd5b850e306bb37e5b2257d7c18
sverona <sasham@fastmail.com>
2019-06-08 20:58:53 -0500

    pare down data model

--- segment/core/entities.py
+++ segment/core/entities.py
@@ -1,67 +1,35 @@
-import enum
-from glob import glob
+#!/usr/bin/env python
+"""The segmenter's data model.
+"""
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import *
-from sqlalchemy.orm import relationship
+from dataclasses import dataclass
+from typing import List
 
-Base = declarative_base()
+@dataclass
+class PortState:
+    """Represents a player's state as a subgroup of SetState.
+    """
+    port_id: int
+    stocks: int
+    pct: float
 
-icons_path = "assets/icons/"
-icons_ext = ".png"
-icons = glob(icons_path + "*" + icons_ext)
-icons = [i[len(icons_path):-len(icons_ext)] for i in icons]
-Characters = enum.Enum("Characters", sorted(icons))
+@dataclass
+class SetState:
+    """Represents a timestamp from a set.
+    """
+    timestamp: float
+    ports: List[PortState]
 
+@dataclass
+class Set:
+    """Represents a Melee set.
+    """
+    id: int
+    states: List[SetState]
 
-class Set(Base):
-    __tablename__ = "set"
-    id = Column(Integer, primary_key=True)
-    games = relationship("Game")
-
-
-class Game(Base):
-    __tablename__ = "game"
-    id = Column(Integer, primary_key=True)
-    set_id = Column(Integer, ForeignKey("set.id"))
-    time_indices = relationship("TimeIndex")
-    ports = relationship("Port")
-    sources = relationship("GameSource")
-
-
-class Source(Base):
-    __tablename__ = "source"
-    id = Column(Integer, primary_key=True)
-    filename = Column(String)
-
-
-class GameSource(Base):
-    __tablename__ = "game_source"
-    game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
-    source_id = Column(Integer, ForeignKey("source.id"), primary_key=True)
-    source = relationship("Source")
-
-
-class Port(Base):
-    __tablename__ = "port"
-    game_id = Column(Integer, ForeignKey("game.id"), primary_key=True)
-    port_number = Column(Integer, primary_key=True)
-    portstates = relationship("PortState")
-
-
-class PortState(Base):
-    __tablename__ = "portstate"
-    id = Column(Integer, primary_key=True)
-    port_id = Column(Integer, ForeignKey("port.id"))
-
-    char = Column(Enum(Characters))
-    stocks = Column(Integer)
-    pct = Column(Integer)
-
-
-class TimeIndex(Base):
-    __tablename__ = "timeindex"
-    id = Column(Integer, primary_key=True)
-    time = Column(Numeric)
-    game_id = Column(Integer, ForeignKey("game.id"))
-    port_states = Column(Integer, ForeignKey("portstate.id"))
+@dataclass
+class Source:
+    """Represents a video object from which a Melee set may be parsed.
+    """
+    id: int
+    video_slug: str
