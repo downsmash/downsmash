867219dd15ab62b89fc7f320f883aee5fbf76695
S. Verona Malone <sverona@sverona.xyz>
2021-10-15 17:35:33 -0500

    add another end-to-end test

--- test/test_watcher.py
+++ test/test_watcher.py
@@ -3,7 +3,7 @@ import pytest
 from downsmash.watcher import watch
 
 
-class TestSegment:
+class TestWatcher:
     def test_ice1(self):
         with pytest.raises(RuntimeError, match=r".*gapless.*"):
             watch("data/ice1.mp4")
@@ -15,3 +15,6 @@ class TestSegment:
     def test_smuckers(self):
         with pytest.raises(RuntimeError, match=r".*gapless.*"):
             watch("data/smuckers.mp4")
+
+    def test_samus_dittos(self):
+        watch("data/samus_dittos.mp4")
