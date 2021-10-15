5d57d5f68cf43346b6f23a97d06c081190768b54
S. Verona Malone <sverona@sverona.xyz>
2021-10-15 06:49:27 -0500

    add test for majority vote

--- test/test_segment.py
+++ test/test_segment.py
@@ -1,9 +1,17 @@
 import pytest
 
-from downsmash.segmenter import Segmenter
 from downsmash.watcher import watch
+
 
 class TestSegment:
     def test_ice1(self):
         with pytest.raises(RuntimeError, match=r".*gapless.*"):
             watch("data/ice1.mp4")
+
+    def test_ice2(self):
+        with pytest.raises(RuntimeError, match=r".*gapless.*"):
+            watch("data/ice2.mp4")
+
+    def test_smuckers(self):
+        with pytest.raises(RuntimeError, match=r".*gapless.*"):
+            watch("data/smuckers.mp4")
